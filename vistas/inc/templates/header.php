e76b5123e0dca776960fd26c48f0ef639a0f8f24
diego10j <diego@MacBook-Pro-de-Diego.local>
2020-01-30 22:15:09 -0500

    index

--- vistas/inc/templates/header.php
+++ vistas/inc/templates/header.php
@@ -19,9 +19,21 @@
 <body>
     <!-- Navegacion-->
     <!-- As a heading -->
-    <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-            AppCursos
-        </a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">AppCursos</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    </nav>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="/vistas/registro.php">Registrarse</a>
+      </li>
+    </ul>
+    <div class="form-inline my-2 my-lg-0">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Iniciar sesión</button>
+    </div>
+  </div>
+</nav>
